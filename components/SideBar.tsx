a4e27f9337b22e460b9b11ab41558eda3ba4624e
Dev Dork <robertcjohnson1984@gmail.com>
2023-02-24 00:15:22 -0700

    create chatrow

--- components/SideBar.tsx
+++ components/SideBar.tsx
@@ -1,10 +1,12 @@
+import NewChat from "./NewChat"
+
 function SideBar() {
     return (
         <div className="p-2 flex flex-col h-screen">
             <div className="flex-1">
                 <div>
-                    {/* NewCHat */}
-                    
+                    <NewChat />
+
                     <div>
                         {/* ModelSelection */}
                     </div>
